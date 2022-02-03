35aee95351457a723121a483c733b4c28146f97a
Francisco García <franciscojavgm@gmail.com>
2022-02-02 19:01:12 -0500

    update/ database funcionality 2

---
+++ app/src/main/java/com/hexagonal/cazarpatos/Jugador.kt
@@ -0,0 +1,5 @@
+package com.hexagonal.cazarpatos
+
+data class Jugador (var usuario:String, var patosCazados:Int){
+    constructor():this("",0)
+}
---
+++ app/src/main/java/com/hexagonal/cazarpatos/RankingActivity.kt
@@ -0,0 +1,46 @@
+package com.hexagonal.cazarpatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hexagonal.cazarpatos.database.RankingPlayerDBHelper
+
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking)
+
+        OperacionesSqLite()
+        GrabarRankingSQLite()
+        LeerRankingsSQLite()
+    }
+    fun OperacionesSqLite(){
+        RankingPlayerDBHelper(this).deleteAllRanking()
+        RankingPlayerDBHelper(this).insertRankingByQuery(Jugador("Jugador9",10))
+        val patosCazados = RankingPlayerDBHelper(this).readDucksHuntedByPlayer("Jugador9")
+        RankingPlayerDBHelper(this).updateRanking(Jugador("Jugador9",5))
+        RankingPlayerDBHelper(this).deleteRanking("Jugador9")
+        RankingPlayerDBHelper(this).insertRanking(Jugador("Jugador9",7))
+        val players = RankingPlayerDBHelper(this).readAllRankingByQuery()
+    }
+    fun GrabarRankingSQLite(){
+        val jugadores = arrayListOf(
+            Jugador("Jugador1", 10),
+            Jugador("Jugador3", 3),
+            Jugador("Jugador4", 9)
+        )
+        jugadores.sortByDescending { it.patosCazados }
+        for(jugador in jugadores){
+            RankingPlayerDBHelper(this).insertRanking(jugador)
+        }
+    }
+    fun LeerRankingsSQLite(){
+        val jugadoresSQLite = RankingPlayerDBHelper(this).readAllRanking()
+        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking)
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this)
+        recyclerViewRanking.adapter = RankingAdapter(jugadoresSQLite)
+        recyclerViewRanking.setHasFixedSize(true)
+    }
+}
+
